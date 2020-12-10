bc36d29d6101a18b9f25afbf593132f36d7a13ba
iRuxu <rx6@qq.com>
2020-12-10 22:58:37 +0800

    update tv

--- src/components/Me.vue
+++ src/components/Me.vue
@@ -10,6 +10,7 @@
         </div>
 
         <div class="m-author-info">
+            <!-- TODO:等级 -->
             <span class="u-name">{{ data.name || "匿名" }}</span>
             <span class="u-uid">UID : {{ data.uid || 0 }}</span>
             <span class="u-join" v-if="data.created_at"
@@ -20,24 +21,62 @@
             <div class="u-bio">{{ data.bio }}</div>
 
             <div class="u-links" v-if="data.weibo_name || data.github_name">
-                <a
-                    v-if="data.weibo_name"
-                    class="u-weibo"
-                    :href="data.weibo_url"
-                    target="_blank"
-                    ><img svg-inline src="../assets/img/weibo.svg" />{{
-                        data.weibo_name
-                    }}</a
-                >
-                <a
-                    v-if="data.github_name"
-                    class="u-github"
-                    :href="data.github_url"
-                    target="_blank"
-                    ><img svg-inline src="../assets/img/github.svg" />{{
-                        data.github_name
-                    }}</a
-                >
+                <el-row :gutter="20">
+                    <el-col :span="6"
+                        ><div>
+                            <a
+                                v-if="data.weibo_name"
+                                class="u-weibo"
+                                :href="data.weibo_url"
+                                target="_blank"
+                                ><img
+                                    svg-inline
+                                    src="../assets/img/weibo.svg"
+                                />{{ data.weibo_name }}</a
+                            >
+                        </div></el-col
+                    >
+                    <el-col :span="6"
+                        ><div>
+                            <a
+                                v-if="data.github_name"
+                                class="u-github"
+                                :href="data.github_url"
+                                target="_blank"
+                                ><img
+                                    svg-inline
+                                    src="../assets/img/github.svg"
+                                />{{ data.github_name }}</a
+                            >
+                        </div></el-col
+                    >
+                    <el-col :span="6"
+                        ><div>
+                            <span
+                                v-if="data.tuilan_id"
+                                class="u-tuilan"
+                                title="推栏ID"
+                            >
+                                <img src="../assets/img/tuilan.png" />推栏ID：{{
+                                    data.tuilan_id
+                                }}
+                            </span>
+                        </div></el-col
+                    >
+                    <el-col :span="6"
+                        ><div>
+                            <a
+                                v-if="data.tv_type && data.tv_id"
+                                class="u-tv"
+                                :href="tv_link"
+                                target="_blank"
+                                ><img :src="tv_img" />直播间：{{
+                                    data.tv_id
+                                }}</a
+                            >
+                        </div></el-col
+                    >
+                </el-row>
             </div>
         </div>
 
@@ -46,8 +85,8 @@
 </template>
 
 <script>
-import { showAvatar } from "@jx3box/jx3box-common/js/utils";
-import { default_avatar } from "@jx3box/jx3box-common/js/jx3box";
+import { showAvatar, getTVlink } from "@jx3box/jx3box-common/js/utils";
+import { default_avatar, __imgPath } from "@jx3box/jx3box-common/js/jx3box";
 import dateFormat from "../utils/dateFormat";
 export default {
     name: "Me",
@@ -62,6 +101,12 @@ export default {
             return this.data.avatar
                 ? showAvatar(this.data.avatar, "l")
                 : default_avatar;
+        },
+        tv_link: function() {
+            return getTVlink(this.data.tv_type, this.data.tv_id) || "";
+        },
+        tv_img: function() {
+            return __imgPath + "image/tv/" + this.data.tv_type + ".png";
         },
     },
     filters: {
