9f1f14cb943eb4bf988573055efa0ee434918657
2734529268@qq.com <2734529268@qq.com>
2021-03-22 19:57:55 +0800

    上传代代码“

--- src/views/Birthday.vue
+++ src/views/Birthday.vue
@@ -1,8 +1,23 @@
 <template>
     <div class="m-birthday">
-
-        贺卡
-
+        <div class="m-letter">
+            <div class="u-head">
+                <div class="u-font"><span>123456</span></div>
+                <div class="u-atv">
+                    <img
+                        class="i-atv"
+                        src="https://console.cnyixun.com/upload/avatar/2020/6/10/7678164.jpg"
+                    />
+                    <span class="i-stamp"></span>
+                </div>
+            </div>
+            <div class="u-cont">
+                <div class="u-title">祝<b>澄江一曲@天鹅坪</b>侠士</div>
+                <div class="u-age"><span>23</span></div>
+            </div>
+        </div>
+        <img src="../assets/img/birthday/light.png" alt="" />
+        <img src="../assets/img/birthday/txt.png" alt="" />
     </div>
 </template>
 
