2b0f3fc0780538a08ca4db6a8a53409799e0eb93
iRuxu <rx6@qq.com>
2022-03-31 00:35:52 +0800

    fix:styles

--- src/components/Me.vue
+++ src/components/Me.vue
@@ -16,21 +16,21 @@
         <div class="m-author-info">
             <span class="u-name">
                 {{ data.display_name || "匿名" }}
-                <span
-                    class="u-superauthor"
-                    title="签约作者"
-                    v-if="isSuperAuthor"
-                >
-                    <img :src="super_author_icon" alt="superauthor" />
-                </span>
-                <span class="u-vip" v-if="isPRO || isVIP" :title="vipTypeTitle">
-                    <i class="i-icon-vip on">{{ vipType }}</i>
-                </span>
+                <el-tooltip content="签约作者" v-if="isSuperAuthor" placement="top">
+                    <span class="u-superauthor">
+                        <img :src="super_author_icon" alt="superauthor" />
+                    </span>
+                </el-tooltip>
             </span>
             <div>
                 <span class="u-uid">UID : {{ data.ID || 0 }}</span>
-                <el-tooltip :content="`当前经验 ${data.experience || 0}`">
+                <el-tooltip :content="`当前经验 ${data.experience || 0}`" placement="top">
                     <span class="u-level">Lv.{{ level }}</span>
+                </el-tooltip>
+                <el-tooltip :content="vipTypeTitle" v-if="isPRO || isVIP" placement="top">
+                    <a class="u-vip" href="/vip/premium?from=user_homepage" target="_blank">
+                        <i class="i-icon-vip on">{{ vipType }}</i>
+                    </a>
                 </el-tooltip>
             </div>
             <span class="u-join" v-if="data.user_registered">
@@ -78,20 +78,15 @@
                         <div>
                             <span v-if="data.tuilan_id" class="u-tuilan" title="推栏ID">
                                 <img src="../assets/img/tuilan.png" />
-                                推栏ID：{{data.tuilan_id}}
+                                推栏ID：{{ data.tuilan_id }}
                             </span>
                         </div>
                     </el-col>
                     <el-col :span="6">
                         <div>
-                            <a
-                                v-if="data.tv_type && data.tv_id"
-                                class="u-tv"
-                                :href="tv_link"
-                                target="_blank"
-                            >
+                            <a v-if="data.tv_type && data.tv_id" class="u-tv" :href="tv_link" target="_blank">
                                 <img :src="tv_img" />
-                                直播间：{{data.tv_id}}
+                                直播间：{{ data.tv_id }}
                             </a>
                         </div>
                     </el-col>
@@ -100,11 +95,11 @@
 
             <div class="m-author-teams">
                 <el-row :gutter="20">
-                    <el-col :span="6" v-for="(item,i) in teams" :key="i">
+                    <el-col :span="6" v-for="(item, i) in teams" :key="i">
                         <div>
                             <a class="u-team" :href="item.team_id | teamLink">
                                 <img class="u-team-logo" :src="item.team_logo | showTeamLogo" />
-                                <span class="u-team-name">{{item.team_name}}</span>
+                                <span class="u-team-name">{{ item.team_name }}</span>
                             </a>
                         </div>
                     </el-col>
@@ -125,7 +120,7 @@ import User from "@jx3box/jx3box-common/js/user";
 export default {
     name: "Me",
     props: [],
-    data: function () {
+    data: function() {
         return {
             // data: {},
             medals: [],
@@ -153,112 +148,108 @@ export default {
         };
     },
     computed: {
-        uid: function () {
+        uid: function() {
             return this.$store.state.uid;
         },
-        data: function () {
+        data: function() {
             return this.$store.state.userdata;
         },
-        avatar: function () {
+        avatar: function() {
             return this.data.user_avatar || "";
         },
-        avatar_frame: function () {
+        avatar_frame: function() {
             return this.data.user_avatar_frame || "";
         },
-        tv_link: function () {
+        tv_link: function() {
             return tvLink(this.data.tv_type, this.data.tv_id) || "";
         },
-        tv_img: function () {
+        tv_img: function() {
             return __imgPath + "image/tv/" + this.data.tv_type + ".png";
         },
-        frameName: function () {
-            return this.avatar_frame && this.frames[this.avatar_frame]
-                ? this.avatar_frame
-                : "";
+        frameName: function() {
+            return this.avatar_frame && this.frames[this.avatar_frame] ? this.avatar_frame : "";
         },
-        frameUrl: function () {
+        frameUrl: function() {
             if (this.frameName) {
                 let fileName = this.frames[this.frameName].files.l.file;
                 return __imgPath + `image/avatar/${this.frameName}/${fileName}`;
             }
             return "";
         },
-        isCircle: function () {
-            return (
-                this.frameName && this.frames[this.frameName].style == "circle"
-            );
+        isCircle: function() {
+            return this.frameName && this.frames[this.frameName].style == "circle";
         },
-        isPRO: function (){
-            return this.data?.is_pro
+        isPRO: function() {
+            return this.data?.is_pro;
         },
-        isSuperAuthor: function (){
-            return !!this.data?.sign
+        isSuperAuthor: function() {
+            return !!this.data?.sign;
         },
-        vipType: function () {
+        vipType: function() {
             return this.isPRO ? "PRO" : "PRE";
         },
-        vipTypeTitle: function () {
+        vipTypeTitle: function() {
             return this.isPRO ? "专业版会员用户" : "高级版会员用户";
         },
-        super_author_icon: function () {
+        super_author_icon: function() {
             return __imgPath + "image/user/" + "superauthor.svg";
         },
-        level: function (){
-            return User.getLevel(this.data?.experience || 0)
-        }
+        level: function() {
+            return User.getLevel(this.data?.experience || 0);
+        },
     },
     filters: {
         time: (val) => {
             return dateFormat(new Date(val));
         },
-        showMedalIcon: function (val) {
+        showMedalIcon: function(val) {
             return __imgPath + "image/medals/user/" + val + ".gif";
         },
-        showMedalDesc : function (item){
-            return item.medal_desc || medal_map[item.medal]
+        showMedalDesc: function(item) {
+            return item.medal_desc || medal_map[item.medal];
         },
-        showAvatar: function (val) {
+        showAvatar: function(val) {
             return showAvatar(val, 180);
         },
-        teamLink: function (val) {
+        teamLink: function(val) {
             return getLink("org", val);
         },
-        showTeamLogo: function (val) {
+        showTeamLogo: function(val) {
             return showAvatar(val, 16);
         },
-        getWeiboLink: function (val) {
+        getWeiboLink: function(val) {
             return "https://weibo.com/" + val;
         },
-        getGithubLink: function (val) {
+        getGithubLink: function(val) {
             return "https://github.com/" + val;
         },
     },
     methods: {
-        init: function () {
+        init: function() {
             this.loadFrames();
             this.loadMedals();
             this.loadTeams();
         },
-        loadMedals: function () {
+        loadMedals: function() {
             if (!this.uid) return;
             getUserMedals(this.uid).then((res) => {
                 this.medals = res.data.data;
             });
         },
-        loadFrames: function () {
+        loadFrames: function() {
             getFrames().then((res) => {
                 if (res.data) {
                     this.frames = res.data || [];
                 }
             });
         },
-        loadTeams: function () {
+        loadTeams: function() {
             getUserPublicTeams(this.uid).then((data) => {
                 this.teams = data || [];
             });
         },
     },
-    mounted: function () {
+    mounted: function() {
         if (this.uid) {
             this.init();
         }
