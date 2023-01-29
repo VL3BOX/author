405dc7d9717a7fbf31cda6a96f4339921912ffc4
yumg <mail>
2023-01-29 12:45:23 +0800

    * fix：适配主题装扮新接口形式

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <template>
     <div id="app">
         <Header></Header>
-        <Breadcrumb
-            :name="name"
-            slug="author"
-            :root="root"
-            :feedbackEnable="true"
-            :withoutLeft="true"
-            v-if="isAdmin"
-        >
+        <Breadcrumb :name="name" slug="author" :root="root" :feedbackEnable="true" :withoutLeft="true" v-if="isAdmin">
             <img slot="logo" svg-inline src="./assets/img/logo.svg" />
             <Info :uid="uid" :userdata="userdata" />
         </Breadcrumb>
         <div class="m-theme" :style="themeStyle">
-            <div class="m-author" v-if="uid" :class="isAdmin?'m-author-admin':''">
+            <div class="m-author" v-if="uid" :class="isAdmin ? 'm-author-admin' : ''">
                 <Me />
                 <Footer></Footer>
             </div>
@@ -25,7 +18,7 @@
 import Me from "@/components/newComponents/Me.vue";
 import Info from "@/components/Info.vue";
 import { getRewrite } from "@jx3box/jx3box-common/js/utils";
-import { getUserInfo,getDecoration,getDecorationJson } from "@/service/cms";
+import { getUserInfo, getDecoration, getDecorationJson } from "@/service/cms";
 import User from "@jx3box/jx3box-common/js/user";
 import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
 export default {
@@ -34,7 +27,7 @@ export default {
     data: function () {
         return {
             isAdmin: User.getInfo().group >= 128,
-            themeStyle:{}
+            themeStyle: {},
         };
     },
     computed: {
@@ -57,112 +50,113 @@ export default {
             getUserInfo(this.uid).then((res) => {
                 this.$store.state.userdata = res.data.data;
             });
-            this.getDecoration()
+            this.getDecoration();
         }
-        
     },
     components: {
         Info,
-        Me
+        Me,
     },
     methods: {
-        showDecoration:function(val,type){
+        showDecoration: function (val, type) {
             return __imgPath + `decoration/images/${val}/${type}.png`;
         },
         //获取装扮,动态获取uid的装扮，缓存指定UID
         getDecoration() {
-            let decoration_local = sessionStorage.getItem('decoration_me'+this.uid);
+            let decoration_local = sessionStorage.getItem("decoration_me" + this.uid);
             if (decoration_local) {
                 //解析本地缓存
-                if(decoration_local == 'no'){
+                if (decoration_local == "no") {
                     return;
                 }
-                let decoration_parse = JSON.parse(decoration_local)
+                let decoration_parse = JSON.parse(decoration_local);
                 if (decoration_parse) {
-                    this.setDecoration(decoration_parse)
+                    this.setDecoration(decoration_parse);
                     return;
                 }
             }
-            getDecoration({using:1,user_id:this.uid}).then(res => {
-                let decorationList=res.data.data
-                if(decorationList.length==0){
+            getDecoration({ using: 1, user_id: this.uid, type: "homebg" }).then((res) => {
+                let decorationList = res.data.data;
+                if (decorationList.length == 0) {
                     //空 则为无主题，不再加载接口，Me界面设No
-                    sessionStorage.setItem('decoration_me'+this.uid,'no')
+                    sessionStorage.setItem("decoration_me" + this.uid, "no");
                     return;
                 }
-                let type=[],decorationName=null
-                for(let i=0;i<decorationList.length;i++){
-                    type.push(decorationList[i].type)
-                    if(i==0){
-                        decorationName=decorationList[i].val
-                    }
-                }
-                let decorationJson=sessionStorage.getItem('decoration_json')
-                if(!decorationJson){
+                let decoration = decorationList[0];
+                let decorationJson = sessionStorage.getItem("decoration_json");
+                if (!decorationJson) {
                     //加载远程json
-                    getDecorationJson().then(json => {
-                        let decoration_json = json.data
-                        let theme = JSON.parse(JSON.stringify(decoration_json[decorationName]))
-                        theme.type=type
-                        this.setDecoration(theme)
-                        sessionStorage.setItem('decoration_json',JSON.stringify(decoration_json))
-                    })
-                }else{
-                    let theme=JSON.parse(decorationJson)[decorationName]
-                    theme.type=type
-                    this.setDecoration(theme)
+                    getDecorationJson().then((json) => {
+                        let decoration_json = json.data;
+                        let theme = JSON.parse(JSON.stringify(decoration_json[decoration.val]));
+                        this.setDecoration(theme);
+                        sessionStorage.setItem("decoration_json", JSON.stringify(decoration_json));
+                    });
+                } else {
+                    let theme = JSON.parse(decorationJson)[decoration.val];
+                    this.setDecoration(theme);
                 }
-            })
+            });
         },
-        setDecoration(theme){
-            sessionStorage.setItem('decoration_me'+this.uid,JSON.stringify(theme))
-            if(theme.type.indexOf('homebg') !== -1){
-                let bgImg=[],w=document.body.offsetWidth
-                // if(!theme.homebg_rb){
-                //     bgImg.push('url('+this.showDecoration('0_TESTSAMPLE','homebg_rb')+') bottom right no-repeat fixed')
-                // }
-                // 背景进行4位置判断,homebg_lt>homebg_rt>homebg_lb>homebg_rb
-                if(theme.homebg_lt){
-                    if(w>1920){
-                        bgImg.push('url('+this.showDecoration(theme.name,'homebg_lt@2x')+') top left no-repeat fixed')
-                    }else{
-                        bgImg.push('url('+this.showDecoration(theme.name,'homebg_lt@1x')+') top left no-repeat fixed')
-                    }
+        setDecoration(theme) {
+            sessionStorage.setItem("decoration_me" + this.uid, JSON.stringify(theme));
+            let bgImg = [],
+                w = document.body.offsetWidth;
+            // if(!theme.homebg_rb){
+            //     bgImg.push('url('+this.showDecoration('0_TESTSAMPLE','homebg_rb')+') bottom right no-repeat fixed')
+            // }
+            // 背景进行4位置判断,homebg_lt>homebg_rt>homebg_lb>homebg_rb
+            if (w > 1920) {
+                if (theme.homebg_lt) {
+                    bgImg.push("url(" + this.showDecoration(theme.name, "homebg_lt@2x") + ") top left no-repeat fixed");
                 }
-                if(theme.homebg_rt){
-                    if(w>1920){
-                        bgImg.push('url('+this.showDecoration(theme.name,'homebg_rt@2x')+') top right no-repeat fixed')
-                    }else{
-                        bgImg.push('url('+this.showDecoration(theme.name,'homebg_rt@1x')+') top right no-repeat fixed')
-                    }
+                if (theme.homebg_rt) {
+                    bgImg.push(
+                        "url(" + this.showDecoration(theme.name, "homebg_rt@2x") + ") top right no-repeat fixed"
+                    );
                 }
-                if(theme.homebg_lb){
-                    if(w>1920){
-                        bgImg.push('url('+this.showDecoration(theme.name,'homebg_lb@2x')+') bottom left no-repeat fixed')
-                    }else{
-                        bgImg.push('url('+this.showDecoration(theme.name,'homebg_lb@1x')+') bottom left no-repeat fixed')
-                    }
+                if (theme.homebg_lb) {
+                    bgImg.push(
+                        "url(" + this.showDecoration(theme.name, "homebg_lb@2x") + ") bottom left no-repeat fixed"
+                    );
                 }
-                if(theme.homebg_rb){
-                    if(w>1920){
-                        bgImg.push('url('+this.showDecoration(theme.name,'homebg_rb@2x')+') bottom right no-repeat fixed')
-                    }else{
-                        bgImg.push('url('+this.showDecoration(theme.name,'homebg_rb@1x')+')  bottom right no-repeat fixed')
-                    }
+                if (theme.homebg_rb) {
+                    bgImg.push(
+                        "url(" + this.showDecoration(theme.name, "homebg_rb@2x") + ") bottom right no-repeat fixed"
+                    );
                 }
-                this.themeStyle={
-                    'background':bgImg.toString(),
+            } else {
+                if (theme.homebg_lt) {
+                    bgImg.push("url(" + this.showDecoration(theme.name, "homebg_lt@1x") + ") top left no-repeat fixed");
+                }
+                if (theme.homebg_rt) {
+                    bgImg.push(
+                        "url(" + this.showDecoration(theme.name, "homebg_rt@1x") + ") top right no-repeat fixed"
+                    );
+                }
+                if (theme.homebg_lb) {
+                    bgImg.push(
+                        "url(" + this.showDecoration(theme.name, "homebg_lb@1x") + ") bottom left no-repeat fixed"
+                    );
+                }
+                if (theme.homebg_rb) {
+                    bgImg.push(
+                        "url(" + this.showDecoration(theme.name, "homebg_rb@1x") + ") bottom right no-repeat fixed"
+                    );
                 }
             }
-        }
-    }
+            this.themeStyle = {
+                background: bgImg.toString(),
+            };
+        },
+    },
 };
 </script>
 
 <style lang="less" scoped>
 @import "./assets/css/app.less";
-@import './assets/css/post.less';
-.m-theme{
+@import "./assets/css/post.less";
+.m-theme {
     // .mt(64px);
     min-height: calc(100vh - @header-height);
     // min-height:100vh;
